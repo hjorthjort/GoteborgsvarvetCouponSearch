ee65291893dbea0f8de4471050ee8474cbce95f2
hjorthjort <rikard.hjort@gmail.com>
2019-03-06 11:39:31 +0100

    Async version

--- explore.py
+++ explore.py
@@ -5,15 +5,30 @@ import string
 
 chars = string.ascii_lowercase
 
-paths = []
-paths.extend([c for c in string.ascii_lowercase])
-while True:
-    for path in paths:
-        # print("Trying " + path)
-        resp = requests.get("https://goteborgsvarvet.propublik.se/"+path, allow_redirects=False)
-        if resp.status_code == 200:
-            print(path)
-    extension = []
-    for path in paths:
-        extension.extend([ c + path for c in chars ])
-    paths = extension
+def wrapper(path):
+    return requests.get("https://goteborgsvarvet.propublik.se/"+path, allow_redirects=False)
+
+async def main():
+    paths = []
+    paths.extend([c for c in string.ascii_lowercase])
+    while True:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+            loop = asyncio.get_event_loop()
+            futures = [
+                loop.run_in_executor(
+                    executor,
+                    wrapper,
+                    path)
+                for path in paths
+            ]
+        for response in await asyncio.gather(*futures):
+            if response.status_code == 200:
+                print (response.url)
+
+        extension = []
+        for path in paths:
+            extension.extend([ c + path for c in chars ])
+        paths = extension
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
